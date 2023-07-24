2fb5db7efd73898428a8cfc9cdc566007c8d650c
Anwar <anwarhussaini160@gmail.com>
2023-07-24 13:57:33 +0430

    Add routes for page navigation

---
+++ src/components/LocalRoutes.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+// import RocketContainer from './RocketContainer';
+// import MissionContainer from './MissionContainer';
+// import ProfileContainer from './ProfileContainer';
+
+export default function LocalRoutes() {
+  return (
+    <main className="container">
+      <Routes>
+        <Route path="/" element={<h2>Rockets</h2>} />
+        <Route path="/missions" element={<h2>Missions</h2>} />
+        <Route path="/profile" element={<h2>My Profile</h2>} />
+      </Routes>
+    </main>
+  );
+}
