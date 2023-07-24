39c9081b92731b7301d1e36a812ac937aecc77c9
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-24 14:27:55 +0300

    Add missions container in local routes

--- src/components/LocalRoutes.jsx
+++ src/components/LocalRoutes.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 // import RocketContainer from './RocketContainer';
-// import MissionContainer from './MissionContainer';
+import MissionContainer from './MissionContainer';
 // import ProfileContainer from './ProfileContainer';
 
 export default function LocalRoutes() {
@@ -8,7 +8,7 @@ export default function LocalRoutes() {
     <main className="container">
       <Routes>
         <Route path="/" element={<h2>Rockets</h2>} />
-        <Route path="/missions" element={<h2>Missions</h2>} />
+        <Route path="/missions" element={<MissionContainer />} />
         <Route path="/profile" element={<h2>My Profile</h2>} />
       </Routes>
     </main>
