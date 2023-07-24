e11ce6efab365007678d385d089b82833a7284a1
Anwar <anwarhussaini160@gmail.com>
2023-07-24 12:58:59 +0430

    Set logo and brand name for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ export default function Header() {
     <header className="container p-3 border-bottom shadow fixed-top text-bg-success mt-2 rounded">
       <nav className="d-flex align-items-center justify-content-between">
         <div className="d-flex gap-2 align-items-center">
-          Logo and Brand
+          <img id="logo" src="./planet.png" alt="Planet Logo" />
+          <h2>Space Travelers Hub</h2>
         </div>
         <ul className="list-group list-group-horizontal">
           Menu Items
