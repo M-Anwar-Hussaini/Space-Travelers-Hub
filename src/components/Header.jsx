2c92d0bf52092b0aa0b1f0b1c2971a91501e2a93
Anwar <anwarhussaini160@gmail.com>
2023-07-24 12:48:59 +0430

    Add brand names and item list structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,14 @@
 export default function Header() {
   return (
     <header className="container p-3 border-bottom shadow fixed-top text-bg-success mt-2 rounded">
-      Header
+      <nav className="d-flex align-items-center justify-content-between">
+        <div className="d-flex gap-2 align-items-center">
+          Logo and Brand
+        </div>
+        <ul className="list-group list-group-horizontal">
+          Menu Items
+        </ul>
+      </nav>
     </header>
   );
 }
