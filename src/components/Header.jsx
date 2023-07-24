b85ed9932983982d3fc220a700ec67750a882e0e
Anwar <anwarhussaini160@gmail.com>
2023-07-24 12:47:02 +0430

    Add header element

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+// import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="container p-3 border-bottom shadow fixed-top text-bg-success mt-2 rounded">
+      Header
+    </header>
+  );
+}
