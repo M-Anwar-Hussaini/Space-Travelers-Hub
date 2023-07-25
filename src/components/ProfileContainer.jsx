e9dfc36c03cb2fb81c9fe6f0edd68f5a421220e0
Anwar <anwarhussaini160@gmail.com>
2023-07-25 08:00:19 +0430

    Add profile container to the Routes

--- src/components/ProfileContainer.jsx
+++ src/components/ProfileContainer.jsx
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux';
+
 export default function ProfileContainer() {
+  const { rockets } = useSelector((store) => store.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.isReserved);
   return (
     <section>
       <div className="row gx-3">
         <div className="col">
-          <div className="card">
-            ProfileRockets
-          </div>
+          <h3>Reserved Rockets</h3>
+          <ul className="list-group">
+            {reservedRockets.map((rocket) => <li className="list-group-item" key={rocket.id}>{rocket.name}</li>)}
+          </ul>
         </div>
         <div className="col">
           <div className="card">
