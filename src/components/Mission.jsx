705e0ab390c97f5eafc8333cdbb17085e214e438
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-25 06:49:38 +0300

    Switch badges for Missions - Conditional

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { join } from '../redux/missions/missionSlice';
+import { join, leave } from '../redux/missions/missionSlice';
 
 export default function Mission({
   name, description, status, id,
@@ -12,23 +12,30 @@ export default function Mission({
       <td>{name}</td>
       <td>{description}</td>
       <td>
-        <span className="badge text-bg-primary">Active member</span>
-        <span className="badge text-bg-secondary">Not a member</span>
+        {status ? (
+          <span className="badge text-bg-primary">Active member</span>
+        ) : (
+          <span className="badge text-bg-secondary">Not a member</span>
+        )}
       </td>
       <td className="text-center col-2">
-        <button
-          type="button"
-          className="btn btn-outline-danger"
-        >
-          Leave Mission
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-secondary"
-          onClick={() => dispatch(join(id))}
-        >
-          Join Mission
-        </button>
+        {status ? (
+          <button
+            type="button"
+            className="btn btn-outline-danger"
+            onClick={() => dispatch(leave(id))}
+          >
+            Leave Mission
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-outline-secondary"
+            onClick={() => dispatch(join(id))}
+          >
+            Join Mission
+          </button>
+        )}
       </td>
     </tr>
   );
