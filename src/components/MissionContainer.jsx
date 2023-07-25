96b850d70849b120e3032fbd8111ced41b713a00
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-25 04:50:22 +0300

    Import mission into missions container

--- src/components/MissionContainer.jsx
+++ src/components/MissionContainer.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Mission from './Mission';
+
 export default function MissionContainer() {
   return (
     <div>
@@ -12,28 +15,7 @@ export default function MissionContainer() {
           </tr>
         </thead>
         <tbody>
-          <tr className="table-body">
-            <td>name</td>
-            <td>description</td>
-            <td>
-              <span className="badge text-bg-primary">Active member</span>
-              <span className="badge text-bg-secondary">Not a member</span>
-            </td>
-            <td className="text-center col-2">
-              <button
-                type="button"
-                className="btn btn-outline-danger"
-              >
-                Leave Mission
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary"
-              >
-                Join Mission
-              </button>
-            </td>
-          </tr>
+          <Mission />
         </tbody>
       </table>
     </div>
